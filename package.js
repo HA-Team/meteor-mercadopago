0b26c55bd590699c8f1332a14e755e6fcde1f993
andruli <andresb9163@gmail.com>
2015-09-02 16:23:06 -0300

    Minor bugfix to allow publishing as a package

--- package.js
+++ package.js
@@ -12,7 +12,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.use('meteorhacks:async', ['server']);
+  api.use('meteorhacks:async@1.0.0', ['server']);
   api.addFiles('mercadopago.js', ['server']);
   api.export('MercadoPago');
 });
